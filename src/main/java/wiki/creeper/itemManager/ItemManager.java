f1b5120e298b9e28fc2c97c1c716a086f68d3781
RangGames <admin@rang.games>
2025-07-26 01:55:16 +0900

    feat: Initialize ItemManager plugin and components (master)
    This commit introduces the core initialization logic for the ItemManager plugin.
    
    It includes the following changes:
    - Adds the `plugin.yml` file with basic plugin information.
    - Implements the `onEnable` method in `ItemManager.java` to:
        - Set the plugin instance.
        - Initialize the `ItemManagerAPI`.
        - Register the `ItemEventListener`.
        - Start the `ExpiredItemCheckTask`.
    - Implements the `onDisable` method to cancel the `ExpiredItemCheckTask`.
    - Adds getter methods for the plugin instance and API.

--- src/main/java/wiki/creeper/itemManager/ItemManager.java
+++ src/main/java/wiki/creeper/itemManager/ItemManager.java
@@ -1,17 +1,45 @@
 package wiki.creeper.itemManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import wiki.creeper.itemManager.api.ItemManagerAPI;
+import wiki.creeper.itemManager.api.impl.ItemManagerImpl;
+import wiki.creeper.itemManager.listener.ItemEventListener;
+import wiki.creeper.itemManager.task.ExpiredItemCheckTask;
 
 public final class ItemManager extends JavaPlugin {
+    
+    private static ItemManager instance;
+    private ItemManagerAPI itemManagerAPI;
+    private ExpiredItemCheckTask expiredItemCheckTask;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        
+        itemManagerAPI = new ItemManagerImpl(this);
+        
+        getServer().getPluginManager().registerEvents(new ItemEventListener(this, itemManagerAPI), this);
+        
+        expiredItemCheckTask = new ExpiredItemCheckTask(this, itemManagerAPI, 20L * 60);
+        expiredItemCheckTask.start();
+        
+        getLogger().info("ItemManager has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (expiredItemCheckTask != null) {
+            expiredItemCheckTask.cancel();
+        }
+        
+        getLogger().info("ItemManager has been disabled!");
+    }
+    
+    public static ItemManager getInstance() {
+        return instance;
+    }
+    
+    public ItemManagerAPI getItemManagerAPI() {
+        return itemManagerAPI;
     }
 }
